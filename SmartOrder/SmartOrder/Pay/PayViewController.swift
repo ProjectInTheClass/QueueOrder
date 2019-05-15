1b8d0f899ee07e5481b592ca62c211bed61ccfe4
Junho Choi <jeff112630@gmail.com>
2019-05-15 14:58:12 +0900

    MyMenu, Pay & Order modified

--- SmartOrder/SmartOrder/Pay/PayViewController.swift
+++ SmartOrder/SmartOrder/Pay/PayViewController.swift
@@ -24,8 +24,6 @@ class PayViewController: UIViewController {
     func goBuy(){
         vc = BootpayController()
         
-        items = cart(selectedMenu: [order1])
-        items.selectedMenu.append(order2)
         
         var userInfo : [String: String] = [
             "name" : "최준호",
@@ -37,10 +35,25 @@ class PayViewController: UIViewController {
             "callbackParam2": "value34",
             "callbackParam3": "value56",
             "callbackParam4": "value78",
-            ]
+        ]
+        
+        for menu in items.selectedMenu{
+            let item = BootpayItem()
+            item.item_name = menu.coffee
+            item.price = Double(menu.price)
+            item.qty = menu.count
+            item.unique = String(menu.coffee.hashValue)
+            if menu.size != "Small"{
+                item.item_name += " 사이즈 업"
+            }
+            if menu.shot != 0{
+                item.item_name += " 샷 \(menu.shot)추가"
+            }
+            vc.price += item.price
+            vc.items.append(item)
+        }
         
         vc.params{
-            $0.price = 0
             $0.name = "Cafe Queue"
             $0.pg = "kakao"
             $0.phone = userInfo["phone"]!
@@ -51,15 +64,6 @@ class PayViewController: UIViewController {
             $0.order_id = "1234"
             $0.quotas = [0];
             
-        }
-        for menu in items.selectedMenu{
-            let item = BootpayItem()
-            item.item_name = menu.coffee
-            item.price = Double(menu.price)
-            item.qty = menu.count
-            item.unique = String(menu.coffee.hashValue)
-            vc.price += item.price
-            vc.items.append(item)
         }
         self.present(vc, animated: true, completion: nil)
     }
@@ -78,10 +82,17 @@ extension PayViewController : BootpayRequestProtocol{
     func onClose() {
         print("close")
         vc.dismiss()
+        self.navigationController?.popViewController(animated: true)
     }
     
     func onConfirm(data: [String : Any]) {
         print(data)
+        
+        //이 부분은 후에 서버 구축 후 카페에서
+        //주문을 받을 수 있는 상태인지 아닌지에 따라
+        //주문을 가능하게 할지, 아닌지를 결정 할 수
+        //있도록 하여 후에 서버의 답변에 따라
+        //isValid를 설정해 두면 된다.
         
         var isValid = true
         if isValid{
@@ -92,11 +103,12 @@ extension PayViewController : BootpayRequestProtocol{
     }
     
     func onCancel(data: [String : Any]) {
-        print(data)
+        print("cancel\n\(data)")
     }
     
     func onDone(data: [String : Any]) {
         print(data)
+        self.navigationController?.popToRootViewController(animated: true)
     }
     
 }
